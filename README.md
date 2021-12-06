e7a4782dacadee440fd18bdd8b9d4478400fe8ef
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-12-06 17:55:46 +0100

    selection a city for user CHECK

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## installation
 Créé un env.js à la racine du dossier avec une variable apiKey qui aura en valeur la votre clef API de https://openweathermap.org, et exporter cette variable.
 
-Pour que cette exportable et requise dans un autre fichier nous avons besoin d'utiliser un bundle(browserify). Lancez dans votre terminal la commande "browserify -e app.js -o dist/bundle.js".
+Pour que cette variable soit exportable et requise dans un autre fichier nous avons besoin d'utiliser un bundle(browserify). Lancez dans votre terminal la commande "browserify -e app.js -o dist/bundle.js".
 
 Une fois ces manipulations effectuées vous pouvez utiliser cette application en local.
 ____
--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 const apiKey = require('./env.js');
 console.log(apiKey);
-let city = "paris"
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
+let city = "";
 
 const app = {
     init: function () {
+        document.querySelector('.search__form').addEventListener('submit', (event) => handleValueCity(event))
         const getData = async () => {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=fr&appid=${apiKey}`;
             try {
                 const response = await fetch(url, {
                     method: 'GET',
                 });
+                console.log(url)
                 const data = await response.json();
                 console.log("response", data);
+                document.querySelector('#city').textContent = data.name;
+                document.querySelector('#temp').textContent = data.main.temp;
+                document.querySelector('#humidity').textContent = `${data.main.humidity}%`;
+                document.querySelector('#wind').textContent = data.wind.speed;
+                if (data.main.temp <= 0) {
+                    document.querySelector('.container').setAttribute("style", "background-image:url('./images/neige.jpg');")
+                }
+                if (data.rain) {
+                    document.querySelector('.container').setAttribute("style", "background-image:url('./images/pluie.jpg');")
+                }
             } catch (err) { console.log(err) }
         }
-        getData();
+        const handleValueCity = (event) => {
+            event.preventDefault();
+            console.log(event.target[0].value)
+            city = event.target[0].value;
+
+            getData();
+        }
+
+
+
+
     }
+
 };
 
 //cahrgement du DOM avant l'application
