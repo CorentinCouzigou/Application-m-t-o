a46a0fc5660e1e80ed060bb6f77647e1ed1610bc
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-12-23 12:41:32 +0100

    webpack in progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,41 @@
 const Dotenv = require('dotenv-webpack');
 const TerserPlugin = require("terser-webpack-plugin");
+const { Resolver } = require('webpack');
+
+const path = require('path');
+// const PATHS = {
+//   img: path.join(dirname, './img'),
+// }
+
 module.exports = {
   mode: 'production',
-  entry: './app.js',
+  entry: ['./app.js', './validation/cityValidation.js'],
   output: {
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  // watch: true,
+  module: {
+    // loaders: [
+    //   {
+    //     test: /\.(png|jpg|jpeg|gif|svg)$/i,
+    //     loader: 'file-loader'
+    //   }
+    // ],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      { test: /ignore\.(png|jpg|gif|svg)$/, loader: 'ignore-loader' },
+      { test: /\.(png|jpg|gif|svg)$/, loader: 'file-loader', options: { name: '[name].[ext]?[hash]' } },
+    ],
   },
   optimization: {
     minimize: true,
- 
   },
-  plugins: [
-    new Dotenv()
-  ]
 }
